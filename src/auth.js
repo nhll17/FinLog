26e92f944359049e19775bd8b5a91ed7d15be9e6
nhll17 <mdl22cs141@mec.ac.in>
2025-08-01 00:56:41 +0530

    createde auth.js,budjet.js and expense.js

---
+++ src/auth.js
@@ -0,0 +1,32 @@
+import {
+  auth,
+  db,
+  createUserWithEmailAndPassword,
+  doc,
+  setDoc,
+  signInWithEmailAndPassword,
+} from "./firebase.js";
+
+export async function signupUser(name, email, password) {
+  const userCred = await createUserWithEmailAndPassword(auth, email, password);
+  await setDoc(doc(db, "users", userCred.user.uid), {
+    profile: { name, email },
+  });
+}
+
+export async function loginUser(email, password) {
+  const userCred = await signInWithEmailAndPassword(auth, email, password);
+  return userCred.user.uid;
+}
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    window.location.href = "dashboard.html";
+  } catch (error) {
+    alert(error.message);
+  }
+});
